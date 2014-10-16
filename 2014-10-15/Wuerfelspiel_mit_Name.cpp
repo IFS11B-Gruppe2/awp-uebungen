d7b7b96a1777c031f912a451dea691252c78da2d
Sergio Lindo <sergiolindo.empresa@gmail.com>
2014-10-16 10:25:45 +0200

    Wuerfelspiel: mehrere Spieler mit Namen

--- 2014-10-15/Wuerfelspiel_mit_Name.cpp
+++ 2014-10-15/Wuerfelspiel_mit_Name.cpp
@@ -3,29 +3,39 @@
 #include <time.h>
 #define N_PLAYERS 8
 #define MAX_VALUE 6
-#define THROWS 6
+#define THROWS 10
 
 using namespace std;
 
+struct T_Wuerfeln {
+  char cName[20];
+  int aiValues[THROWS];
+};
+
 int main() {
   // vars
-  int aiValues[N_PLAYERS][THROWS];
-  int aiTimes[MAX_VALUE] = {0};
+  T_Wuerfeln Spieler[N_PLAYERS];
+  int aiTimes[MAX_VALUE + 1] = {0};
 
-  // logic
+  /* beginning of logic */
+
+  // Get random numbers.
   for(int player_number = 0; player_number < N_PLAYERS; player_number++) {
     cout << "Name: ";
-    cin >> aiValues[player_number]
-    for(int throw_number = 0; throw_number < 10; throw_number++) {
-      aiValues[player_number][throw_number] = rand() % 6 + 1;
+    cin >> Spieler[player_number].cName;
+    for(int throw_number = 0; throw_number < THROWS; throw_number++) {
+      Spieler[player_number].aiValues[throw_number] = rand() % MAX_VALUE + 1;
     }
   }
 
+  // Get number occurrences
   for(int player_number = 0; player_number < N_PLAYERS; player_number++) {
-    for(int throw_number = 0; throw_number < 10; throw_number++) {
-      aiTimes[aiValues[player_number][throw_number] - 1] ++;
+    for(int throw_number = 0; throw_number < THROWS; throw_number++) {
+      aiTimes[Spieler[player_number].aiValues[throw_number]] ++;
     }
   }
+
+  /* end of logic */
 
   // output
   cout << "--------------------------------------" << endl;
@@ -35,18 +45,18 @@ int main() {
 
   cout << "Die gewürfelten Zahlen der Spieler 1-" << N_PLAYERS << ":" << endl;
   for(int player_number = 0; player_number < N_PLAYERS; player_number++) {
-    cout << "Spieler " << player_number + 1 << ":  ";
+    cout << "Spieler " << Spieler[player_number].cName << ":  ";
 
     for(int throw_number = 0; throw_number < THROWS; throw_number++) {
-      cout << aiValues[player_number][throw_number] << " ";
+      cout << Spieler[player_number].aiValues[throw_number] << " ";
     }
     cout << endl;
   }
   cout << endl << endl;
 
-  cout << "Ziehungshäufigkeiten der Zahlen 1-6:" << endl;
-  for(int throw_number = 0; throw_number < MAX_VALUE; throw_number++) {
-    cout << "Die Zahl " << throw_number + 1 << "  " << aiTimes[throw_number] << " mal" << endl;
+  cout << "Ziehungshäufigkeiten der Zahlen 1-" << MAX_VALUE << ":" << endl;
+  for(int throw_number = 1; throw_number <= MAX_VALUE; throw_number++) {
+    cout << "Die Zahl " << throw_number << "  " << aiTimes[throw_number] << " mal" << endl;
   }
   cout << endl;
 
