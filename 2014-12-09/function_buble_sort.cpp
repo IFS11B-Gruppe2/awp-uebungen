16e51d9eb36948ace0753ef77d8ca40deef2be9d
Sergio Lindo <sergiolindo.empresa@gmail.com>
2014-12-09 19:24:15 +0100

    Add function to sort numbers (ASC): Bubble sort

---
+++ 2014-12-09/function_buble_sort.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <stdio.h>
+#define LENGTH_ACZEICHEN 4
+#define LENGTH_AIFELD 3
+
+using namespace std;
+
+void fill(char *pac, int *pai);
+void showIntArray(int *pai, int paiLenght);
+void sortIntArray(int *pai, int paiLenght);
+
+int main() {
+	char acZeichen[LENGTH_ACZEICHEN] = "WER";
+	int aiFeld[LENGTH_AIFELD] = {0};
+
+	fill(acZeichen, aiFeld);
+
+	printf("acZeichen: %s \n", acZeichen);
+	cout << "aiFeld: ";
+	showIntArray(aiFeld, LENGTH_AIFELD);
+	cout << endl;
+	sortIntArray(aiFeld, LENGTH_AIFELD);
+	cout << "aiFeld(geordnet): ";
+	showIntArray(aiFeld, LENGTH_AIFELD);
+	cout << endl;
+}
+
+void fill(char *pac, int *pai) {
+	for (int i = 0; *(pac + i) != '\0'; i++) {
+		*(pai + i) = *(pac + i);
+	}
+}
+
+void showIntArray(int *pai, int paiLenght) {
+	for(int i = 0; i < paiLenght; i++) {
+		cout << pai[i] << " ";
+	}
+}
+
+void sortIntArray(int *pai, int paiLenght) {
+	bool sorted;
+	int i = 0;
+
+	do {
+		++i;
+		sorted = true;
+
+		for (int k = 0; k < paiLenght - 1; ++k) {
+			if (pai[k] > pai[k + 1]) {
+				int tmp = pai[k];
+				pai[k] = pai[k + 1];
+				pai[k + 1] = tmp;
+				sorted = false;
+			}
+		}
+	} while(i < paiLenght && !sorted);
+}
