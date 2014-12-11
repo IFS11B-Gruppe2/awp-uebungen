05c7f260528469c03380b6f9dd293d0af63e11ce
Sergio Lindo <sergiolindo.empresa@gmail.com>
2014-12-11 15:21:58 +0100

    Add MaxMin().

--- 2014-12-11/Mittelwert_dynamische_Speicherverwaltung.cpp
+++ 2014-12-11/Mittelwert_dynamische_Speicherverwaltung.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 void Eingabe(int *piFeld, int iAnzahl);
 float Mittelwert(int *piFeld, int iAnzahl);
+void MaxMin(int *piFeld, int iAnzahl, int &iMax, int &iMin);
 int Max(int *piFeld, int iAnzahl);
 int Min(int *piFeld, int iAnzahl);
 int* reserviertInt(int iAnzahl);
@@ -20,8 +21,9 @@ int main() {
 
 	Eingabe(piWerte, iAnzahl);
 	fMittelwert = Mittelwert(piWerte, iAnzahl);
-	iMax = Max(piWerte, iAnzahl);
-	iMin = Min(piWerte, iAnzahl);
+	//iMax = Max(piWerte, iAnzahl);
+	//iMin = Min(piWerte, iAnzahl);
+	MaxMin(piWerte, iAnzahl, iMax, iMin);
 
 	cout << "Mittelwert: " << fMittelwert << endl;
 	cout << "Max: " << iMax << endl;
@@ -37,6 +39,21 @@ void Eingabe(int *piFeld, int iAnzahl) {
 	for(int i = 0; i < iAnzahl; ++i) {
 		cout << "[" << i << "]: ";
 		cin >> piFeld[i];
+	}
+}
+
+void MaxMin(int *piFeld, int iAnzahl, int &iMax, int &iMin) {
+	iMax = piFeld[0]; // first number
+	iMin = piFeld[0]; // first number
+
+	for(int i = 1; i < iAnzahl; ++i) {
+		if (piFeld[i] > iMax) {
+			iMax = piFeld[i];
+		}
+
+		if (piFeld[i] < iMin) {
+			iMin = piFeld[i];
+		}
 	}
 }
 
