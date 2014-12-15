09317f94294243553f99216e1c3a1d1e81208a94
Sergio Lindo <sergiolindo.empresa@gmail.com>
2014-12-15 20:16:54 +0100

    Add resizeArray().

--- 2014-12-15/dinamische_Reservierung.cpp
+++ 2014-12-15/dinamische_Reservierung.cpp
@@ -3,13 +3,20 @@
 using namespace std;
 
 void copyArray(int *paiSource, int iHowMany, int *paiTarget);
+void resizeArray(int *pai, int &iCurrentLength);
 void showIntArray(int *pai, int paiLength);
 
 int main() {
-	int test1[3] = {1,2,3}, test2[3];
+	int iArrayLenght = 3,
+	    *test = new int[iArrayLenght];
 
-	copyArray(test1, 3, test2);
-	showIntArray(test2, 3);
+	test[0] = 0;
+	test[1] = 1;
+	test[2] = 2;
+
+	resizeArray(test, iArrayLenght);
+	test[3] = 3;
+	showIntArray(test, iArrayLenght);
 	cout << endl;
 
 	return 0;
@@ -19,6 +26,19 @@ void copyArray(int *paiSource, int iHowMany, int *paiTarget) {
 	for (int i = 0; i < iHowMany; ++i) {
 		paiTarget[i] = paiSource[i];
 	}
+}
+
+void resizeArray(int *pai, int &iCurrentLength) {
+	int *tmp = pai;
+
+	pai = new int[iCurrentLength + 1];
+
+	copyArray(tmp, iCurrentLength, pai);
+
+	++iCurrentLength;
+
+	delete[] tmp;
+	tmp = NULL;
 }
 
 void showIntArray(int *pai, int paiLength) {
