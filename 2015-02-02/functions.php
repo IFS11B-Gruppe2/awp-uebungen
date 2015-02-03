f1377e445f90a1eab4af62e2a57493064268630d
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-02-03 14:05:53 +0100

    Zahlenausgaben_Tabellen: Add performance

--- 2015-02-02/functions.php
+++ 2015-02-02/functions.php
@@ -4,7 +4,7 @@ function is_prime($number) {
 	$has_divisor = false;
 
 	if ($number != 2) {
-		for ($i = 2; $i < $number; $i++) {
+		for ($i = 2; $i < $number / 2 and $has_divisor == false; $i++) {
 			if ($number % $i == 0) {
 				$has_divisor = true;
 			}
