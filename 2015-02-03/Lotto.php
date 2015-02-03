ff0c139a5a5f4f46cb573f2f541a85fa0608083f
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-02-03 20:26:10 +0100

    Lotto: Computer recommends numbers

--- 2015-02-03/Lotto.php
+++ 2015-02-03/Lotto.php
@@ -1,8 +1,16 @@
+<?php
+$recommended = array();
+for ($i = 0; $i < 6; $i++) {
+	$recommended[$i] = rand(1, 49);
+}
+?>
 <!DOCTYPE html>
 <html lang="de">
 <head>
 	<meta charset="UTF-8">
 	<title>Lotto</title>
+
+	<link rel="stylesheet" type="text/css" href="Lotto.css">
 </head>
 <body>
 	<table>
@@ -10,7 +18,18 @@
 			<?php for ($row = 0, $number = 1; $row < 7; $row++) { ?>
 			<tr>
 				<?php for ($cell = 0; $cell < 7; $cell++, $number++) { ?>
-				<td><?php echo $number; ?></td>
+				<?php
+					if (in_array($number, $recommended)) {
+						$class = 'recommended';
+					} else {
+						$class = '';
+					}
+				?>
+				<td>
+					<span class="<?php echo $class; ?>">
+						<?php echo $number; ?>
+					</span>
+				</td>
 				<?php } ?>
 			</tr>
 			<?php } ?>
