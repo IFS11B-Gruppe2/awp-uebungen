b05cba5fbb03937737225310e30f96f047bf1ccd
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-02-05 13:57:30 +0100

    Lotto: No repeat recommended number, fix

--- 2015-02-03/Lotto.php
+++ 2015-02-03/Lotto.php
@@ -6,11 +6,11 @@ for ($game_row = 0; $game_row < 6; $game_row++) {
 	for ($game_column = 0; $game_column < 2; $game_column++) {
 		$recommended[$game_row][$game_column] = array();
 		for ($i = 0; $i < 6; $i++) {
-			$random = rand(1, 49);
+			do {
+				$random = rand(1, 49);
+			} while (in_array($random, $recommended[$game_row][$game_column]));
 
-			if (!in_array($random, $recommended[$game_row][$game_column])) {
-				$recommended[$game_row][$game_column][$i] = $random;
-			}
+			$recommended[$game_row][$game_column][$i] = $random;
 		}
 	}
 }
