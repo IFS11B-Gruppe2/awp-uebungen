8a613c9cea3ab84c0f6978445584aabfe1e2ec04
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-03-17 14:21:42 +0100

    PHP: Taschenrechner - Summe

---
+++ 2015-03-16/Taschenrechner.php
@@ -0,0 +1,69 @@
+<?php
+	session_start('calculator');
+
+	if (!isset($_POST['btnCalculate'])) {
+		$txtN1 = 0;
+		$txtN2 = 0;
+		$result = 0;
+	} else {
+		$txtN1 = $_POST['txtN1'];
+		$txtN2 = $_POST['txtN2'];
+
+		$result = $txtN1 + $txtN2;
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Taschenrechner</title>
+	<link rel="stylesheet" type="text/css" href="Taschenrechner.css" />
+</head>
+<body>
+
+<h1>Taschenrechner</h1>
+
+<form method="POST">
+  <table>
+    <tbody>
+      <tr>
+        <td>Operand 1</td>
+
+        <td>Operator</td>
+
+        <td>Operand 2</td>
+
+        <td> </td>
+
+        <td>Ergebnis</td>
+      </tr>
+
+      <tr>
+        <td>
+          <input type="text" name="txtN1" value="<?php echo $txtN1; ?>" />
+        </td>
+
+        <td>+</td>
+
+        <td>
+          <input type="text" name="txtN2" value="<?php echo $txtN2; ?>" />
+        </td>
+
+        <td>=</td>
+
+        <td>
+          <input type="text" name="txtResult" value="<?php echo $result; ?>" />
+        </td>
+      </tr>
+
+      <tr>
+        <td colspan="5">
+          <input type="submit" name="btnCalculate" value="Berechnen" class="wide"/>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</form>
+
+</body>
+</html>
