c938093f5221cce39f11ca998586cfc5e9febfbb
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-03-26 15:40:50 +0100

    SQL: Query Aufgaben

--- 2015-03-24/Aufgaben.sql
+++ 2015-03-24/Aufgaben.sql
@@ -79,3 +79,100 @@ SELECT A_Art, COUNT(A_Nr)
 FROM artikel
 GROUP BY 1
 ;
+
+########
+## 10 ##
+########
+SELECT a.A_Bez, a.A_Art, a.A_VK
+FROM artikel a
+WHERE
+	a.A_VK > (
+		SELECT AVG(a2.A_VK)
+		FROM artikel a2
+		WHERE a2.A_Art = a.A_Art
+	)
+;
+
+########
+## 11 ##
+########
+SELECT DISTINCT k.*
+FROM
+	kunden k
+	JOIN bestellungen b ON (b.K_Nr = k.K_Nr)
+WHERE b.B_Erledigt = 0
+;
+
+########
+## 12 ##
+########
+SELECT *
+FROM
+	kunden k
+	JOIN bestellungen b ON (b.K_Nr = k.K_Nr)
+ORDER BY k.K_Name ASC
+;
+
+########
+## 13 ##
+########
+SELECT a.*, bd.B_Menge
+FROM
+	artikel a
+	JOIN bestelldetails bd ON (bd.A_Nr = a.A_Nr)
+WHERE a.A_Art = 'Laserdrucker'
+;
+
+########
+## 14 ##
+########
+SELECT k.K_Name, k.K_Vorname, bd.A_Nr, bd.B_Menge
+FROM
+	kunden k
+	JOIN bestellungen b ON (b.K_Nr = k.K_Nr)
+	JOIN bestelldetails bd ON (bd.B_Nr = b.B_Nr)
+WHERE b.B_Erledigt = 0
+;
+
+########
+## 15 ##
+########
+SELECT k.*
+FROM
+	kunden k
+	JOIN bestellungen b ON (b.K_Nr = k.K_Nr)
+	JOIN bestelldetails bd ON (bd.B_Nr = b.B_Nr)
+	JOIN artikel a ON (a.A_Nr = bd.A_Nr)
+WHERE
+	a.A_Art = 'CD-ROM'
+	AND
+	a.A_VK > 150/1.95583
+;
+
+########
+## 16 ##
+########
+SELECT k.K_Name, k.K_Vorname, k.K_PLZ, k.K_Ort, a.A_Art, a.A_VK
+FROM
+	kunden k
+	JOIN bestellungen b ON (b.K_Nr = k.K_Nr)
+	JOIN bestelldetails bd ON (bd.B_Nr = b.B_Nr)
+	JOIN artikel a ON (a.A_Nr = bd.A_Nr)
+WHERE
+	k.K_PLZ BETWEEN 80000 AND 89999
+;
+
+########
+## 17 ##
+########
+SELECT k.K_Name, k.K_Vorname, a.A_Bez, a.A_Nr
+FROM
+	kunden k
+	JOIN bestellungen b ON (b.K_Nr = k.K_Nr)
+	JOIN bestelldetails bd ON (bd.B_Nr = b.B_Nr)
+	JOIN artikel a ON (a.A_Nr = bd.A_Nr)
+WHERE
+	b.B_Erledigt = 0
+	AND
+	a.A_Bez LIKE 'M%'
+;
