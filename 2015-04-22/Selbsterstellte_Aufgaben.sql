5dd83579401853fef5a31c550da72c08b000ae65
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-04-22 10:54:35 +0200

    SQL: Create own exercises

---
+++ 2015-04-22/Selbsterstellte_Aufgaben.sql
@@ -0,0 +1,17 @@
+## 1
+## Listen Sie die Länder mit ihren Sprachen auf, und ob sie 'offiziel' oder 'nicht offiziel' ist.
+## Absteigend sortiert nach Landname.
+## Offizielle Sprachen sollen vor den nicht offiziellen Sprachen gezeigt werden.
+
+## 2
+## Listen Sie auf, wie viele Personen jeder Sprache sprechen.
+## Absteigend sortiert nach Bürgeranzahl.
+
+## 3
+## Listen Sie auf, wie viele Personen wohnen in unterschiedlichen Regierungsformen.
+## Absteigend sortiert nach Personenanzahl.
+
+## 4
+## Listen Sie die Bürgeranzahl jeder Hauptstadt mit ihren Sprachen.
+## Absteigend sortiert nach Stadtname.
+## Offizielle Sprachen sollen vor den nicht offiziellen Sprachen gezeigt werden.
