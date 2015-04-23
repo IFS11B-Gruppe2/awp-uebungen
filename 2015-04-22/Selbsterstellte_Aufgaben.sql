b88a7082a95c35fe06d3f280a485cdce77644b47
Sergio Lindo <sergiolindo.empresa@gmail.com>
2015-04-23 14:45:27 +0200

    SQL: Aufgabe 1 Lösung

--- 2015-04-22/Selbsterstellte_Aufgaben.sql
+++ 2015-04-22/Selbsterstellte_Aufgaben.sql
@@ -2,7 +2,19 @@
 ## Listen Sie die Länder mit ihren Sprachen auf, und ob sie 'offiziel' oder 'nicht offiziel' sind.
 ## Aufsteigend sortiert nach Landname.
 ## Offizielle Sprachen sollen vor den nicht offiziellen Sprachen gezeigt werden.
-
+## Aufsteigend sortiert nach Sprache.
+SELECT
+  co.Name AS 'Land',
+  cola.Language AS 'Sprache',
+  IF (cola.IsOfficial = 'T', 'Offiziel', 'Nicht offiziel') AS 'Offiziel?'
+FROM
+  country AS co
+  INNER JOIN countryLanguage AS cola ON (cola.CountryCode = co.Code)
+ORDER BY
+  1 ASC,
+  3 DESC,
+  2 ASC
+;
 
 ## 2
 ## Listen Sie auf, wie viele Personen jeder Sprache sprechen.
